edf840234f188b83bb321d69361c8b3b69faeaab
Season <seasonprice326@gmail.com>
2016-01-25 23:44:10 +0000

    added pins scaffold, updated navbar

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,4 +1,13 @@
 class HomeController < ApplicationController
   def index
   end
+  
+  def about
+  end
+  
+  def recipe
+  end
+  
+  
+  
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,9 @@
 Rails.application.routes.draw do
+  resources :pins
+  devise_for :users
   root 'home#index'
+  get 'home/about'
+  get 'home/recipe'
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
